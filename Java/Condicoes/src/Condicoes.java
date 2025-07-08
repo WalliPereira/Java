ffc095cc1bd484da8a8979becbd10ec77471dfdc
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2025-07-08 20:21:19 -0300

    Update Condicoes.java

--- Java/Condicoes/src/Condicoes.java
+++ Java/Condicoes/src/Condicoes.java
@@ -5,10 +5,12 @@ public class Condicoes {
 
         Scanner s= new Scanner(System.in);
 
-        System.out.println("Digite um numero de 0 a 12:  ");
+        // nextByte() para ler um numero em um intervalo entre -128 a 127.
+        // caso o numero não esteja entre esses dara erro.
+        System.out.println("Digite um numero de -12 a 12:  ");
         byte num= s.nextByte();
 
-        if (num == 8) {
+        if (num == 8 || num == -8) {
             System.out.println(("Voce acertou!!"));
         } else {
             System.out.println("Voce digitou o numero errado.");
